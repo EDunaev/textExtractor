fc7e234ec972a1dd22638f86cd05be60f1812fc5
Evgeny Dunaev <evgeny.dunaev@capgemini.com>
2020-02-06 10:26:02 +0100

    removed strings which contain only whitespaces

--- textExtractor.py
+++ textExtractor.py
@@ -7,8 +7,7 @@ root = tree.getroot()
 file = codecs.open("annot.txt", "w", "utf-8")
 textlist = []
 for element in root.iter():
-    if element.text is not None:
+    if element.text is not None and not element.text.isspace():
            textlist.append(element.text)
-
 file.write("\n".join(textlist))
 file.close()
